742eb1d6f2f8709db49fa5ca2ac28591e5faf044
mikok42 <mikolaj@linczewski.com>
2021-04-20 18:48:47 +0200

    computed properties ale na serio vol 2

---
+++ computedProperties.playground/Contents.swift
@@ -0,0 +1,38 @@
+import Foundation
+
+struct Point {
+    var x: Float = 0.0
+    var y: Float = 0.0
+}
+
+struct pointGroup {
+    var points: [Point] = []
+    
+    var centerOfMass: Point {
+        get {
+            guard !points.isEmpty else { return Point(x: 0, y: 0) }
+            
+            var xPos: Float = 0
+            var yPos: Float = 0
+            
+            points.forEach { (point) in
+                xPos += point.x
+                yPos += point.y
+            }
+            
+            return Point(x: xPos/Float(points.count), y: yPos/Float(points.count))
+        }
+    }
+}
+
+var group = pointGroup()
+
+(1...10).forEach { (i) in
+    group.points.append(Point(x: Float.random(in: -10...10), y: Float.random(in: -10...10)))
+}
+
+print(group.centerOfMass)
+
+group.points.append(Point(x: 1.0, y: 1.0))
+
+print(group.centerOfMass)
