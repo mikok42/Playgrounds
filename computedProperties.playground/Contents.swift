dc5a2fce76126b3a12b305a6ab9518fc02739e5d
mikok42 <mikolaj@linczewski.com>
2021-04-20 23:09:37 +0200

    map

--- computedProperties.playground/Contents.swift
+++ computedProperties.playground/Contents.swift
@@ -15,10 +15,12 @@ struct pointGroup {
             var xPos: Float = 0
             var yPos: Float = 0
             
-            points.forEach { (point) in
-                xPos += point.x
-                yPos += point.y
-            }
+            points.map { xPos += $0.x ; yPos = $0.y }
+            
+//            points.forEach { (point) in
+//                xPos += point.x
+//                yPos += point.y
+//            }
             
             return Point(x: xPos/Float(points.count), y: yPos/Float(points.count))
         }
